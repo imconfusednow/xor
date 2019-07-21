3be37d2434d32f89fd676f2b431f6d73eb8f82eb
Ben Allport <ben_e_allport@hotmail.co.uk>
2019-07-21 20:59:37 +0100

    add neat to file path

--- xor.py
+++ xor.py
@@ -1,6 +1,10 @@
-from population import Population
-from network import Network
-from dataconf import DataConf
+import sys
+sys.path.insert(0, 'H:\\Documents\\Coding_projects')
+sys.path.insert(1, 'H:\\Documents\\Coding_projects\\neat')
+
+from neat import Population
+from neat import Network
+from neat import DataConf
 import csv
 import pygame
 
